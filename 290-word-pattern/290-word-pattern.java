381a8ca909bb238e904ae2060d7e92b66e5b8658
Himanshu Gupta <himbxr22@gmail.com>
2022-01-17 19:17:48 +0530

    Time: 1 ms (86.67%), Space: 38.7 MB (20.23%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -3,6 +3,7 @@ class Solution {
        
         String[] arr= s.split(" ");
         HashMap<Character, String> map = new HashMap<Character, String>();
+         HashMap<String,Character> check = new HashMap<>();
         if(arr.length!= pat.length())
             return false;
         for(int i=0; i<arr.length; i++){
@@ -12,9 +13,10 @@ class Solution {
                     return false;
             }else{
                 
-                if(map.containsValue(arr[i]))
+                if(check.containsKey(arr[i]))
                     return false;
                 map.put(c, arr[i]);
+                check.put(arr[i],c);
             }    
         }
         return true;
