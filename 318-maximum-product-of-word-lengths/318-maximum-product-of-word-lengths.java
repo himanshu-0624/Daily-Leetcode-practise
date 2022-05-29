222744c6b8d7c8556df88a5ff03f62163c503781
Himanshu Gupta <himbxr22@gmail.com>
2022-05-29 08:56:45 +0530

    Time: 11 ms (71.82%), Space: 42 MB (96.87%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        long [] val = new long[n];
+        
+        for(int i = 0; i<n; i++)
+        {
+            for(char c : words[i].toCharArray())
+            {
+                int pos = c - 'a';
+                long h = 1<<pos;
+                val[i] = (val[i]|h);
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0; i<n; i++)
+        {
+            int v1 = words[i].length();
+            for(int j = i+1; j<n; j++)
+            {
+                int v2 = words[j].length();
+                
+                if((val[i]|val[j]) == (val[i]^val[j]))
+                {
+                  ans = Math.max(ans,v1*v2);   
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
