e5a87bbb0b14ae14c221e8f07733fc0dda7d86a4
Himanshu Gupta <himbxr22@gmail.com>
2023-02-10 08:23:52 +0530

    Added solution - LeetHub

---
+++ Balloon Everywhere - GFG/balloon-everywhere.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.maxInstance(s);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maxInstance(String s) {
+        // Code here
+        int [] b = new int[5];
+        for(char c : s.toCharArray())
+        {
+            if(c == 'b')
+            b[0]++;
+           else if(c == 'a')
+            b[1]++;
+            else if(c == 'n')
+            b[2]++;
+            else if(c == 'l')
+            b[3]++;
+            
+             else if(c == 'o')
+            b[4]++;
+        }
+        
+        b[3]/=2;
+        b[4]/=2;
+        Arrays.sort(b);
+        return b[0];
+    }
+}
