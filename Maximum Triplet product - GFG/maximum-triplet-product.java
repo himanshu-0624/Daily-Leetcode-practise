96a6993d317ae012a6c3035ffa2bf17a5859e29f
Himanshu Gupta <himbxr22@gmail.com>
2023-03-10 08:57:10 +0530

    Added solution - LeetHub

---
+++ Maximum Triplet product - GFG/maximum-triplet-product.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    Long maxTripletProduct(Long arr[], int n)
+    {
+        // Complete the function
+        Long max = Long.MIN_VALUE;
+        Arrays.sort(arr);
+        long v1 = arr[0]*arr[1]*arr[n-1];
+        long v2 = arr[n-1]*arr[n-2]*arr[n-3];
+        max = Math.max(v1,v2);
+        return max;
+    }
+}
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    
+		    Long[] arr = new Long[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Long.parseLong(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    Long res = obj.maxTripletProduct(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+// } Driver Code Ends
